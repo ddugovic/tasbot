c74b46eb4fc26cdf275732bfa613ec15fbafdc0e
Daniel Dugovic <dandydand@gmail.com>
2013-07-03 11:16:08 -0500

    Cleaned up formatting.

--- tasbot/game.h
+++ tasbot/game.h
@@ -4,65 +4,72 @@
 
 /*
 #define GAME "mario"
-#define MOVIE "mario-cleantom.fm2"
-// #define MOVIE "mario-newtom2.fm2"
-// #define FASTFORWARD 256  // XXX cheats! -- should be 0
+#define MOVIE "mario.fm2"
 #define FASTFORWARD 0
 */
 
 /*
 #define GAME "tetris"
-#define MOVIE "tetristom2.fm2"
+#define MOVIE "tetris.fm2"
 #define FASTFORWARD 0
 */
 
 /*
 #define GAME "adventureisland"
-#define MOVIE "adventureisland-tom.fm2"
+#define MOVIE "adventureisland.fm2"
 #define FASTFORWARD 0
 */
 
 /*
 #define GAME "karate"
-#define MOVIE "karate-tom.fm2"
+#define MOVIE "karate.fm2"
 #define FASTFORWARD 0
 */
 
 /*
 #define GAME "dinosaur"
-#define MOVIE "dinosaur-tom.fm2"
+#define MOVIE "dinosaur.fm2"
 #define FASTFORWARD 0
 */
 
 /*
 #define GAME "contra"
-#define MOVIE "contra-tom.fm2"
+#define MOVIE "contra.fm2"
 #define FASTFORWARD 0
 */
 
 /*
 #define GAME "megaman2"
-#define MOVIE "megaman2-tom.fm2"
+#define MOVIE "megaman2.fm2"
 #define FASTFORWARD 0
 */
 
+/*
 #define GAME "gradius"
-#define MOVIE "gradius-tom.fm2"
+#define MOVIE "gradius.fm2"
 #define FASTFORWARD 0
+*/
 
-// #define GAME "pacman"
-// #define MOVIE "pacman-tom.fm2"
-// #define FASTFORWARD 600
+#define GAME "pacman"
+#define MOVIE "pacman.fm2"
+#define FASTFORWARD 600
 
 /*
 #define GAME "tetris"
-#define MOVIE "tetris-tom.fm2"
+#define MOVIE "tetris.fm2"
+#define FASTFORWARD 0
 */
 
 /*
 #define GAME "bubblebobble"
-#define MOVIE "bubblebobble-tom.fm2"
+#define MOVIE "bubblebobble.fm2"
 #define FASTFORWARD 0
+*/
+
+/*
+#define GAME "marblemadness"
+#define MOVIE "marblemadness.fm2"
+#define FASTFORWARD 750
 */
 
 #endif
